a952852558690c280c2963db1c80b4292d082535
Pawel Paruzel <pawelparuzel95@gmail.com>
2018-01-17 14:25:02 +0100

    Randomize function for layers with mean 0.5 Added a few comments

--- multilayer/nn.py
+++ multilayer/nn.py
@@ -15,12 +15,22 @@ class Layer():
         # activation
         return NeuralNetwork.relu(data)
 
+    # 0.5 STARTING BIAS
+    def randomize(self):
+        # randomize synapses
+        for row in range(self.synapses.rows()):
+            for col in range(self.synapses.cols()):
+                self.synapses.m[row][col] = np.random.normal() + 0.5
+            # randomize bias
+            for row in range(self.bias.rows()):
+                self.bias.m[row][0] = np.random.normal() + 0.5
+
     def __repr__(self):
         return "Layer<{!r}>".format(self.synapses)
 
 
 class NeuralNetwork():
-    # Initialize Neural Network with an amount of inputNeurons,
+    # initialize Neural Network with an amount of inputNeurons,
     # hiddenNeurons, outputNeurons, and a learning rate
     def __init__(self, inputN, hiddenN, outputN, learning_rate):
         # learning rate
@@ -33,53 +43,45 @@ class NeuralNetwork():
         # output layer
         self.layers.append(Layer(hiddenN[-1:][0], outputN))
 
+    # randomize synapses and bias
     def randomize(self):
         for layer in self.layers:
-            # randomize synapses
-            for row in range(layer.synapses.rows()):
-                for col in range(layer.synapses.cols()):
-                    layer.synapses.m[row][col] = np.random.normal()
-                # randomize bias
-                for row in range(layer.bias.rows()):
-                    layer.bias.m[row][0] = np.random.normal()
+            layer.randomize()
 
+    # activation function - ReLU
     @staticmethod
-    # TODO: WRONG
     def relu(x):
         if isinstance(x, Matrix):
-            pass
+            for row in range(x.rows()):
+                for col in range(x.cols()):
+                    x.m[row][col] = max(0, x.m[row][col])
+            return x
         else:
-            pass
-        return max(0, x)
+            return max(0, x)
 
+    # loss function - squared error
     def loss(self, prediction, target):
         return (prediction - target) ** 2
 
+    # function propagating value through neural network
     def predict(self, inputData, expectedOutput):
-        data = inputData
         for layer in self.layers:
-            data = layer.feedforward(data)
-        cost = self.loss(data, expectedOutput)
-        return data, cost
+            inputData = layer.feedforward(inputData)
+        cost = self.loss(inputData, expectedOutput)
+        return inputData, cost
 
+    # feedforward and apply backpropagation
     def train(self, inputData, expectedOutput):
         prediction, cost = self.predict(inputData, expectedOutput)
-        print("Pred: {}\tCost: {}".format(prediction, cost))
-        # backpropagation algorithm
+        print("Pred: {}\nCost: {}".format(prediction, cost))
+        # TODO: backpropagation algorithm
+        # ...
 
 
 def main():
-    L1 = Layer(2, 3)
-    L2 = Layer(3, 1)
-    data = Matrix([10, 10])
-    data = L1.feedforward(data)
-    data = L2.feedforward(data)
-    print(data)
-
-    # mlp = NeuralNetwork(2, (2,), 1, learning_rate=0.0001)
-    # mlp.randomize()
-    # mlp.train(Matrix([1, 2]), Matrix([3]))
-    # mlp.predict(Matrix([1, 2])
+    mlp = NeuralNetwork(2, (3,), 1, learning_rate=0.0001)
+    mlp.randomize()
+    mlp.train(Matrix([1, 2]), Matrix([3]))
 
 
 if __name__ == "__main__":
